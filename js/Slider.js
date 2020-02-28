245a00ed4f2a8094f4e72d05970a87bb3adafaf6
Joan Andrés <andres6936@live.com>
2020-02-28 11:50:59 -0500

    Added functionality for the button right

--- js/Slider.js
+++ js/Slider.js
@@ -3,6 +3,11 @@
 class Slider {
 
     constructor() {
+        /**
+         * Determine the slider that actually is showed.
+         * @type {number}
+         */
+        this.sliderActualShowed = 1;
         this.sliderOne = document.getElementById("slider-one");
         this.sliderTwo = document.getElementById("slider-two");
         this.sliderThree = document.getElementById("slider-three");
@@ -12,15 +17,50 @@ class Slider {
         this.sliderOne.style.display = "none";
     }
 
+    showSliderOne() {
+        this.sliderOne.style.display = "table-cell";
+    }
+
     hiddenSliderTwo() {
         this.sliderTwo.style.display = "none";
     }
 
+    showSliderTwo() {
+        this.sliderTwo.style.display = "table-cell";
+    }
+
+    hiddenSliderThree() {
+        this.sliderThree.style.display = "none";
+    }
+
+    showSliderThree() {
+        this.sliderThree.style.display = "table-cell";
+    }
+
+    showNextSlider() {
+        if (this.sliderActualShowed === 1) {
+            this.hiddenSliderOne();
+            this.showSliderTwo();
+            this.sliderActualShowed = 2;
+        } else if (this.sliderActualShowed === 2) {
+            this.hiddenSliderTwo();
+            this.showSliderThree();
+            this.sliderActualShowed = 3;
+        } else if (this.sliderActualShowed === 3) {
+            this.hiddenSliderThree();
+            this.showSliderOne();
+            this.sliderActualShowed = 1;
+        }
+    }
 }
 
 let sliders = new Slider();
 
-sliders.hiddenSliderOne();
 sliders.hiddenSliderTwo();
+sliders.hiddenSliderThree();
+
+function showNextSlider() {
+    sliders.showNextSlider();
+}
 
 console.log("Hey");
