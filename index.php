3d9e9c2ac526f67defe593a84ce04e28fedd0bf4
Joan Andrés <andres6936@live.com>
2020-04-29 10:27:27 -0500

    Added new display flex

--- index.php
+++ index.php
@@ -37,6 +37,30 @@ $var->showNavigation($links);
     </div>
 </div>
 
+<div class="d-md-flex flex-md-equal w-100 my-md-3 pl-md-3">
+    <!-- First Position -->
+    <div class="bg-dark mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-white overflow-hidden">
+        <div class="my-3 py-3">
+            <h2 class="display-5">Another headline</h2>
+            <p class="lead">And an even wittier subheading.</p>
+        </div>
+        <div class="bg-light shadow-sm mx-auto" style="width: 80%; height: 300px;
+            border-radius: 21px 21px 0 0">
+        </div>
+    </div>
+
+    <!-- Second Position -->
+    <div class="bg-light mr-md-3 pt-3 px-3 pt-md-5 px-md-5 text-center text-dark overflow-hidden">
+        <div class="my-3 py-3">
+            <h2 class="display-5">Another headline</h2>
+            <p class="lead">And an even wittier subheading.</p>
+        </div>
+        <div class="bg-dark shadow-sm mx-auto" style="width: 80%; height: 300px;
+            border-radius: 21px 21px 0 0">
+        </div>
+    </div>
+</div>
+
 <!-- Principal Content -->
 <section class="container" id="posts" role="main">
 
