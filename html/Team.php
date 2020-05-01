6c60f50fa04f753d5c9e6392c76daa00c862f72d
Joan Andrés <andres6936@live.com>
2020-05-01 08:51:30 -0500

    Fixed header and navigation on the Team page

--- html/Team.php
+++ html/Team.php
@@ -18,29 +18,13 @@
 </head>
 <body role="document">
 
-<!-- Header and Navigation -->
-<nav class="navbar navbar-expand-lg navbar-light bg-ligth">
-    <a class="navbar-brand" href="#">Vrosky</a>
+<?php
 
-    <div class="collapse navbar-collapse">
-        <ul class="navbar-nav mr-auto">
-            <li class="nav-item active">
-                <a class="nav-link" href="Team.php">Team</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Privacy.php">Privacy</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="Contact.php">Contact</a>
-            </li>
-        </ul>
+$links = ["../php/Register.php", "#", "Privacy.php", "Contact.php"];
 
-        <form class="form-inline my-2 my-lg-0">
-            <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="search">
-            <button class="btn btn-outline-info my-2 my-sm-0" type="submit">Search</button>
-        </form>
-    </div>
-</nav>
+$var = new Template();
+$var->showNavigation($links);
+?>
 
 <!-- Principal Content -->
 <section class="team-section text-center my-5 mx-auto" style="width: 80%">
