5d98aedec950453ab6c28dce34574d4419bc7ff4
Joan Andrés <andres6936@live.com>
2020-05-23 14:03:58 -0500

    Added test-muted to text

--- html/Privacy.php
+++ html/Privacy.php
@@ -37,7 +37,7 @@ $var->showNavigation($links);
     <div class="row py-5">
         <h1 class="col-10 offset-1 text-center">You’re in control</h1>
         <article class="col-8 offset-2 pt-5">
-            <p class="text-center">
+            <p class="text-center text-muted">
                 We know that one size doesn’t fit all
                 when it comes to privacy, so we build
                 powerful, easy-to-use privacy tools
@@ -51,7 +51,7 @@ $var->showNavigation($links);
         <h1 class="col-12 py-5">Control how data is used across Google</h1>
         <div class="col-4">
             <h3>Take control of data saved in your Google Account</h3>
-            <p>You can find key info, privacy, and security
+            <p class="text-muted">You can find key info, privacy, and security
                 settings all in your Google Account. We have
                 created easy-to-use tools like Dashboard and
                 My Activity, which give you transparency over
@@ -68,7 +68,7 @@ $var->showNavigation($links);
         <div class="col-4">
             <h3>Choose the privacy settings that are right for you
                 with your Privacy Checkup</h3>
-            <p>In just a few minutes, you can manage the types
+            <p class="text-muted">In just a few minutes, you can manage the types
                 of data we collect, update what you share with
                 friends or make public, and adjust the types
                 of ads you would like us to show you. You can
