735891534cceda627def77cc1bb792cdeb79a262
Joan Andrés <andres6936@live.com>
2020-06-01 19:17:47 -0500

    Complete question current market

--- js/CurrentMarket.js
+++ js/CurrentMarket.js
@@ -9,6 +9,41 @@ let currentMarket = {
             title: 'Relationship of the good to the need.',
             good: 'It is always needed. It satisfies a basic need.',
             bad: 'Luxury product, not necessary.',
-        }
+        },
+        {
+            title: 'Strength and influence of the competition',
+            good: 'Relatively little competition. Unexplored field.',
+            bad: 'Market dominated by much well-established competition.',
+        },
+        {
+            title: 'Quality - price ratio compared to goods or services of the competition.',
+            good: 'Goods or services with special characteristics. Much better than competing products.',
+            bad: 'True imitation of existing products on the market.',
+        },
+        {
+            title: 'Needs for after-sales services or care (e.g. maintenance).',
+            good: 'easily served good or service. Service system available or easily contracted.',
+            bad: 'Unknown service needs. No service facilities available.',
+        },
+        {
+            title: 'Availability of sales - distribution systems',
+            good: 'Easy to market with existing distributors.',
+            bad: 'Requires special sales - distribution system.',
+        },
+        {
+            title: 'Effort demanded by sales.',
+            good: 'The good or service sells itself.',
+            bad: 'Each sale demands great effort.',
+        },
+        {
+            title: 'Export possibilities.',
+            good: 'Can be exported competitively. Large international market.',
+            bad: 'Only national market.',
+        },
+        {
+            title: '',
+            good: '',
+            bad: '',
+        },
     ]
 }
