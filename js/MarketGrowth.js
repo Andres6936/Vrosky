2c690ba773b2a43d54badd3947bf3b53bba4c1e6
Joan Andrés <andres6936@live.com>
2020-06-01 22:31:14 -0500

    Added new json embedded

---
+++ js/MarketGrowth.js
@@ -0,0 +1,42 @@
+let marketGrowth = [
+    {
+        title: 'Projected increase in the number of consumers',
+        good: 'Trends indicate a growing consumer population.',
+        bad: 'Decreasing consumer population.',
+    },
+    {
+        title: 'Projected increase in needs.',
+        good: 'Projected increase in demand for associated products.',
+        bad: 'Decreasing demand for associated products.',
+    },
+    {
+        title: 'Increased consumer acceptance',
+        good: 'It is certain that the demand will increase when the consumers know the product.',
+        bad: 'It\'s not possible to have many sales. Consumer acceptance will have a minimal effect on sales.',
+    },
+    {
+        title: 'Product novelty and design protection',
+        good: 'New product. Can be protected with patents.',
+        bad: 'Difficult to protect. Can be easily copied.',
+    },
+    {
+        title: 'Economic trends (favorable to increased consumption).',
+        good: 'Projected economic trends will increase demand and/or value',
+        bad: 'Projected economic trends will seriously reduce demand and/or negatively affect costs.',
+    },
+    {
+        title: 'Social and political trends (favourable to the increase of consumption).',
+        good: 'Trends indicate stability and an increase in need.',
+        bad: 'Indicators of instability. Social and political changes may weaken the market.',
+    },
+    {
+        title: 'Competitive advantages.',
+        good: 'High added value. Industry that is not easily entered. Easily protected innovation processes.',
+        bad: 'Low added value. In this field it is easy to start a business.',
+    },
+    {
+        title: '',
+        good: '',
+        bad: '',
+    },
+]
