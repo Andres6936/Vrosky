84e21a4a77b0ac53a79acee2b8b2b206c7ba7ea6
Joan Andrés <andres6936@live.com>
2020-06-01 23:02:33 -0500

    Added new json embedded risks

---
+++ js/Risks.js
@@ -0,0 +1,71 @@
+let risks = [
+    {
+        title: 'Market stability in economic cycles',
+        good: 'The market is not substantially affected. The product or service is required in good and bad times.',
+        bad: 'Demand falls substantially in bad times.',
+    },
+    {
+        title: 'Technological risks.',
+        good: 'Stable technology. If not, the product and processes can be easily modified in response to new technology.',
+        bad: 'Product tied to current technology. Technological advances occur rapidly.',
+    },
+    {
+        title: 'Import competition.',
+        good: 'The nature of the product makes importing impossible There are no visible fears of importation.',
+        bad: 'The product requires the import of raw materials. High labor content. Can be easily shipped over long distances.',
+    },
+    {
+        title: 'Size and power of the competition',
+        good: 'No competitor can substantially affect the distribution of the market.',
+        bad: 'Powerful competitors can at any time, reduce prices to capture market.',
+    },
+    {
+        title: 'Quality and reliability risks (untested design)',
+        good: 'Proven quality and reliability',
+        bad: 'Product design not fully tested.',
+    },
+    {
+        title: 'Ease of demand forecasting',
+        good: 'The estimation of the demand is done with ease and precision. Information quickly available.',
+        bad: 'Unknown reliability. No information available to estimate demand. The estimate is to a high degree a riddle.',
+    },
+    {
+        title: 'Value of initial investments.',
+        good: 'Relatively low investment. Can be liquidated with little or no loss.',
+        bad: 'High investment required. Special buildings or machinery. Can only be liquidated at a large loss.',
+    },
+    {
+        title: 'Vulnerability of inputs (supply and price)',
+        good: 'Raw materials widely available. No shortages expected.',
+        bad: 'Supply of raw materials in decline and closely controlled.',
+    },
+    {
+        title: 'Legislation and controls.',
+        good: 'The product does not affect health or the environment. There is no likelihood of controls.',
+        bad: 'Product in region of controversy There are pending controls.',
+    },
+    {
+        title: 'Time required to make a profit.',
+        good: 'Cash flow projections show gains in the first months of operation.',
+        bad: 'Delayed earnings.',
+    },
+    {
+        title: 'Inventory needs.',
+        good: 'Short distribution chain. Inventory requirements are minimal.',
+        bad: 'Long inventory time of raw materials. Consumer needs require large inventories of finished goods.',
+    },
+    {
+        title: 'Seasonal demand.',
+        good: 'Stable demand in all seasons.',
+        bad: 'Highly seasonal demand. You can\'t have production all the time.',
+    }, {
+        title: 'Design exclusivity.',
+        good: 'Exclusive design that cannot be easily copied.',
+        bad: 'Innovation in design can be done easily. It is not easy to maintain leadership in design.',
+    },
+    {
+        title: '',
+        good: '',
+        bad: '',
+    },
+]
