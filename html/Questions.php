026dde13131f7fa2f8c1c9f61600a912c4c2768e
Joan Andrés <andres6936@live.com>
2020-06-06 13:21:20 -0500

    Added new image

--- html/Questions.php
+++ html/Questions.php
@@ -102,7 +102,7 @@ $var->showNavigation($links);
                     <a class="btn btn-primary text-light font-weight-bold">Understand</a>
                 </div>
                 <div class="col-sm-5 pt-4 pl-5 d-none d-sm-block">
-                    <img class="float-right" src="../img/ManBuss.png" alt="Man" height="350px">
+                    <img class="float-right" src="../img/ManWhite.png" alt="Man" height="350px">
                 </div>
             </div>
         </div>
